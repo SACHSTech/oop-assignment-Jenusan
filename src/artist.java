da415e6e57cd071619c6ddc2fc513e915591801a
jenusan <jenusan.yogarajah23@ycdsbk12.ca>
2022-11-08 18:05:32 -0500

    idk tbh

--- src/artist.java
+++ src/artist.java
@@ -2,8 +2,11 @@ public class artist {
 
     public String name;
     public double rating;
-    public artist(String name, double rating, String from, album ambum){
-
+    public String from;
+    public artist(String name, double rating, String from, album album){
+        this.name = name;
+        this.rating = rating;
+        this.from = from;
     }
     
     public double getRating(){
