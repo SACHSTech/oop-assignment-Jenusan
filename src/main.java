39b9779cfde4474d3ac3a5007ebf7389001eccca
jenusan <jenusan.yogarajah23@ycdsbk12.ca>
2022-11-08 20:53:46 -0500

    nav

--- src/main.java
+++ src/main.java
@@ -7,16 +7,29 @@ public class main {
         BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
         ArrayList<song> List = new ArrayList();
         boolean ask = true;
+        boolean quit = false;
         int count = 0;
 
+        while (quit == false){
+        System.out.println("Press 0 to quit");
+        System.out.println("Press keys 1-4 to navigate");
+        System.out.println("Rate Songs (1) ");
+        System.out.println("Stats      (4) ");
+        int start = Integer.parseInt(key.readLine());
+        if (start == 0){
+            quit = true;
+        }
 
+        if (start == 1){
         while (ask){
-            System.out.println("enter song name: ");
+            System.out.print("input (0) to quit");
+            System.out.print("enter song name: ");
             String song = input.readLine();
             if (song.equals("0")){
+                start = 0;
                 break;
             }
-            System.out.println("enter artist name: ");
+            System.out.print("enter artist name: ");
             String artist = input.readLine();
             System.out.println("enter rating: ");
             double rate = Double.parseDouble(key.readLine());
@@ -24,20 +37,25 @@ public class main {
             song s = new song(song, rate, artist);
             List.add(s);
         }
-
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-
-        System.out.println("Songs ranked from highest to lowest ");
-        for (int i = 100; i >= 0; i--){
-            for (int t = 0; t < List.size(); t++){
-                song now = List.get(t);
-                if (now.getRating()*10 == i){
-                    count++;
-                    System.out.println(count + ". " + now.getName() + " artist: " + now.getArtist()+" rating: " + now.getRating()); 
+        }else if (start == 4){
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("Songs ranked from highest to lowest ");
+            for (int i = 100; i >= 0; i--){
+                for (int t = 0; t < List.size(); t++){
+                    song now = List.get(t);
+                    if (now.getRating()*10 == i){
+                        count++;
+                        System.out.println(count + ". " + now.getName() + " artist: " + now.getArtist()+" rating: " + now.getRating()); 
+                    }
                 }
             }
+            System.out.println("");
+            System.out.println("");
+            start = 0;
+        }
+
         }
 
     }
